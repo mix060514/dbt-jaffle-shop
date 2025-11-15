7059fe30a9c6c0d4cf40588ea9eda2ddf276526b
mix060514@gmail.com <mix060514@gmail.com>
2025-11-15 18:30:11 +0000

    add seed and is_valid to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,1 +1,18 @@
-select * from {{ source('jaffle_shop', 'orders') }}
+with source as (select * from {{ source('jaffle_shop', 'orders') }}),
+
+status_mapping as (select * from {{ ref('seed_order_statuses') }}),
+
+transformed as (
+   select
+       t0.id,
+       t0.user_id,
+       t0.order_date,
+       t0.status,
+       t0._etl_loaded_at,
+       t1.is_valid
+   from source as t0
+   left join status_mapping as t1
+       on t0.status = t1.status
+)
+
+select * from transformed
